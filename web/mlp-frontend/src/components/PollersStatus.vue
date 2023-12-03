74c42c944e4f4c86e226ac792af900a3a2c152bd
codetrasher <jkortelahti@gmail.com>
2023-12-03 02:14:50 +0200

    Add conditional rendering to PollersStatus
    When the component is mounted, it should attempt to establish
    a WebSocket connection to the server for receiving running pollers
    and their status. If pollers are received, an HTML table is populated
    with the data, otherwise an error element is shown. Test the behaviour..

--- web/mlp-frontend/src/components/PollersStatus.vue
+++ web/mlp-frontend/src/components/PollersStatus.vue
@@ -1,20 +1,65 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const runningPollers = ref<Poller[]>([]);
+  
+let client: WebSocket;
+
+interface Poller {
+  site: string;
+  pattern: string;
+  status: string;   // TODO: Could possibly be an enum, as well?
+};
+
+
+onMounted(() => {
+  client = new WebSocket('ws://localhost:1212');
+  client.onopen = () => {
+    console.log('WebSocket connection opened');   // TODO: Replace with proper logging
+    client.send('getRunningPollers');
+  };
+  client.onmessage = (event: MessageEvent<string>) => {
+    let pollers: Poller[] = [];
+
+    try {
+      pollers = <Poller[]>JSON.parse(event.data);
+    } catch(error: any) {
+      console.error(`Error reading websocket data: ${error}`)
+    }
+
+    if (pollers && pollers.length > 0) {
+      runningPollers.value.splice(0, runningPollers.value.length);
+      pollers.map(poller => runningPollers.value.push(poller));
+    }
+  };
+});
 </script>
 
 <template>
   <div>
     <label for="running-pollers-table">Running pollers</label>
-    <table id="running-pollers-table">
-      <thead>
-        <tr>
-          <th>Site</th>
-          <th>Pattern</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody data-test="running-pollers-table-body">
-      </tbody>
-    </table>
+    <div v-if="runningPollers.length > 0" id="running-poller-div">
+      <table id="running-pollers-table" data-test="running-pollers-div">
+        <thead>
+          <tr>
+            <th>Site</th>
+            <th>Pattern</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody data-test="running-pollers-table-body">
+          <tr v-for="poller in runningPollers" :key="poller.site">
+            <td>{{ poller.site }}</td>
+            <td>{{ poller.pattern }}</td>
+            <td>{{ poller.status }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- TODO: Make the error more verbose, 'error-y' -->
+    <div v-else id="no-running-pollers-error" >
+      <p data-test="no-running-pollers-error">No running pollers</p>
+    </div>
   </div>
 </template>
 
--- web/mlp-frontend/test/components/PollersStatus.spec.ts
+++ web/mlp-frontend/test/components/PollersStatus.spec.ts
@@ -1,19 +1,54 @@
 
-import { beforeEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { WS } from 'vitest-websocket-mock';
 import { DOMWrapper, VueWrapper, mount } from '@vue/test-utils';
 import PollersStatus from '../../src/components/PollersStatus.vue';
 
-describe.concurrent('PollersStatus', () => {
+describe('PollersStatus', () => {
 
-    let wrapper: VueWrapper;
-    let tableBodyElement: Omit<DOMWrapper<HTMLElement>, 'exists'>;
+  let wrapper: VueWrapper;
 
-    beforeEach(() => {
-        wrapper = mount(PollersStatus);
-        tableBodyElement = wrapper.get('[data-test="running-pollers-table-body"]');
-    });
+  let fakeWSServer: WS;
 
-    test('should initialise', () => {
-        expect(tableBodyElement.element.children.length).toBe(0);
-    });
+  const pollersTableBody: string = '[data-test="running-pollers-table-body"]';
+  const noRunningPollersError: string = '[data-test="no-running-pollers-error"]';
+
+  beforeEach(() => {
+
+    fakeWSServer = new WS('ws://localhost:1212');
+    wrapper = mount(PollersStatus);
+  });
+
+  afterEach(() => {
+    fakeWSServer.close();
+    WS.clean();
+  });
+
+  test('should receive empty pollers array and show an error element', async () => {
+    await fakeWSServer.connected;
+    await expect(fakeWSServer).toReceiveMessage('getRunningPollers');
+
+    fakeWSServer.send([]);
+
+    await wrapper.vm.$nextTick();
+    
+    let noRunningPollersErrorElement = wrapper.get(noRunningPollersError);
+    expect(noRunningPollersErrorElement.text()).toEqual('No running pollers');
+  }, 2000);
+
+  test('should receive a poller from websocket and show a table with a running pollers', async () => {
+    await fakeWSServer.connected;
+    await expect(fakeWSServer).toReceiveMessage('getRunningPollers');
+
+    fakeWSServer.send(JSON.stringify([{
+      site: 'www.testsite.com',
+      pattern: 'xyz=123',
+      status: 'running'
+    }]));
+
+    await wrapper.vm.$nextTick();
+    
+    let pollersTableBodyElement = wrapper.get(pollersTableBody);
+    expect(pollersTableBodyElement.element.children.length).toEqual(1);
+  }, 2000);
 });
